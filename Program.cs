c94555698b39c61ca4423cdd5a0cecbbe6ac3714
Sampada <sampadahakke102@gmail.com>
2022-01-17 17:23:46 +0530

    Added; Welcome message is added

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticeProblem
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("HelloWorld");
+            //Console.ReadLine();
+
+            //ReverseNumber reverseNumber=new ReverseNumber();
+            //reverseNumber.FindReverseNumber();
+
+            FibonocciSeries fibonocci = new FibonocciSeries();
+            fibonocci.FindFibonocciSeriesNumber();
+
+        }
+    }
+}
---
+++ ReverseNumber.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticeProblem
+{
+    internal class ReverseNumber
+    {
+       
+            public void FindFibonocciSeriesNumber()
+            {
+                Console.Write("please enter how many fibonacci numbers we have to generate");
+                int number = Convert.ToInt32(Console.ReadLine());
+                int first = 0, second = 1;
+                int result = 0;
+                result = first + second;
+                Console.WriteLine("first:{0} second:{1} result:{2}", first, second, result);
+                Console.ReadLine();
+            }
+        
+    }
+}
